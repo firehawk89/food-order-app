51399a6a679a9f2ff993fad835d85129c3e31e48
Bochkovskyi <unundertale1@ukr.net>
2023-03-10 20:15:07 +0200

    update cart logic

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,8 +1,22 @@
-import styles from "./MealItem.module.scss";
+import { useContext } from "react";
+
 import MealItemForm from "./MealItemForm";
+import CartContext from "../../../store/cart-context";
+import styles from "./MealItem.module.scss";
 
 const MealsItem = (props) => {
+  const cartContext = useContext(CartContext);
+
   const price = `$${props.price.toFixed(2)}`;
+
+  const addToCartHandler = (amount) => {
+    cartContext.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
 
   return (
     <li className={styles.meal}>
@@ -12,7 +26,7 @@ const MealsItem = (props) => {
         <span className={styles.price}>{price}</span>
       </div>
       <div>
-        <MealItemForm id={props.id} />
+        <MealItemForm id={props.id} onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
