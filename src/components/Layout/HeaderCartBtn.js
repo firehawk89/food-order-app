8b15fe2500672f4285ef77230a02226eb3629a8f
Bochkovskyi <unundertale1@ukr.net>
2023-03-10 20:58:44 +0200

    add cart btn animation

--- src/components/Layout/HeaderCartBtn.js
+++ src/components/Layout/HeaderCartBtn.js
@@ -1,18 +1,39 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 import styles from "./HeaderCartBtn.module.scss";
 
 const HeaderCartBtn = (props) => {
+  const [buttonIsBumped, setButtonIsBumped] = useState(false);
   const cartContext = useContext(CartContext);
 
-  const numberOfCartItems = cartContext.items.reduce(
+  const { items } = cartContext;
+
+  const numberOfCartItems = items.reduce(
     (currNumber, item) => currNumber + item.amount,
     0
   );
 
+  const buttonStyles = `${styles.button} ${buttonIsBumped ? styles.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+
+    setButtonIsBumped(true);
+
+    const timer = setTimeout(() => {
+      setButtonIsBumped(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={buttonStyles} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
