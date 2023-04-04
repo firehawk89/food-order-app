0a397ba50ed289286261b143a424b80ec466732a
Bochkovskyi <unundertale1@ukr.net>
2023-04-04 20:46:46 +0300

    add custom http hook, connected firebase db

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,54 +1,72 @@
+import { useEffect, useState } from "react";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 import styles from "./AvailableMeals.module.scss";
-
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+import useHttpRequest from "../../hooks/use-http-request";
 
 const AvailableMeals = () => {
-  const mealsList = (
-    <ul>
-      {DUMMY_MEALS.map((meal) => {
-        return (
-          <MealItem
-            id={meal.id}
-            key={meal.id}
-            name={meal.name}
-            description={meal.description}
-            price={meal.price}
-          />
-        );
-      })}
-    </ul>
+  const [meals, setMeals] = useState([]);
+  const { isLoading, error, sendRequest: fetchMeals } = useHttpRequest();
+
+  useEffect(() => {
+    const updateMeals = (mealObj) => {
+      const loadedMeals = [];
+
+      for (const mealKey in mealObj) {
+        loadedMeals.push({
+          id: mealKey,
+          name: mealObj[mealKey].name,
+          description: mealObj[mealKey].description,
+          price: mealObj[mealKey].price,
+        });
+      }
+
+      setMeals(loadedMeals);
+    };
+
+    fetchMeals(
+      {
+        url: "https://food-order-app-80fd0-default-rtdb.europe-west1.firebasedatabase.app/meals.json",
+      },
+      updateMeals
+    );
+  }, [fetchMeals]);
+
+  let mealsList = (
+    <p className={styles["meals__list-fallback"]}>Found no meals</p>
   );
+
+  if (meals.length > 0) {
+    mealsList = (
+      <ul className={styles["meals__list"]}>
+        {meals.map((meal) => {
+          return (
+            <MealItem
+              id={meal.id}
+              key={meal.id}
+              name={meal.name}
+              description={meal.description}
+              price={meal.price}
+            />
+          );
+        })}
+      </ul>
+    );
+  }
+
+  let content = mealsList;
+
+  if (isLoading) {
+    content = <p className={styles["meals__list-fallback"]}>Loading...</p>;
+  }
+
+  if (error) {
+    content = <p className={styles["meals__list-fallback"]}>{error}</p>;
+  }
 
   return (
     <section className={styles.meals}>
-      <Card>{mealsList}</Card>
+      <Card>{content}</Card>
     </section>
   );
 };
